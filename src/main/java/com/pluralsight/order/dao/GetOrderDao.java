fc7e100246322ffc5d069ac8045e618ea3e39805
Eric-W-Chen <xericchenx@gmail.com>
2021-08-12 16:36:40 -0400

    second

--- src/main/java/com/pluralsight/order/dao/GetOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/GetOrderDao.java
@@ -5,7 +5,11 @@ import com.pluralsight.order.dto.ParamsDto;
 import com.pluralsight.order.util.Database;
 import com.pluralsight.order.util.ExceptionHandler;
 
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
+
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -31,12 +35,24 @@ public class GetOrderDao {
      * @return Object with the main information of an order
      */
     public OrderDto getOrderById(ParamsDto paramsDto) {
-        OrderDto orderDto = null;
+        OrderDto orderDto = new OrderDto();
 
-        try (Connection con = null;
+        try (Connection con = database.getConnection();
              PreparedStatement ps = createPreparedStatement(con, paramsDto.getOrderId());
              ResultSet rs = createResultSet(ps)
         ) {
+            OrderDto order_dto = orderDto;
+            while (rs.next()) {
+                long orderID = rs.getLong("order_id");
+                order_dto.setOrderId(orderID);
+                long customerID = rs.getLong("customer_id");
+                order_dto.setCustomerId(customerID);
+                Date orderDate = rs.getDate("order_date");
+                order_dto.setDate(orderDate);
+                String status = rs.getString("order_status");
+                order_dto.setStatus(status);
+            }
+            
 
         } catch (SQLException ex) {
             ExceptionHandler.handleException(ex);
@@ -53,8 +69,9 @@ public class GetOrderDao {
      * @throws SQLException In case of an error
      */
     private PreparedStatement createPreparedStatement(Connection con, long orderId) throws SQLException {
-
-        return null;
+        PreparedStatement prepared_statement = con.prepareStatement(query);
+        prepared_statement.setLong(1, orderId);
+        return prepared_statement;
     }
 
     /**
@@ -64,6 +81,7 @@ public class GetOrderDao {
      * @throws SQLException In case of an error
      */
     private ResultSet createResultSet(PreparedStatement ps) throws SQLException {
-        return null;
+        ResultSet result_set = ps.getResultSet();
+        return result_set;
     }
 }
--- src/main/java/com/pluralsight/order/util/ExceptionHandler.java
+++ src/main/java/com/pluralsight/order/util/ExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.pluralsight.order.util;
 
+import java.io.Console;
 import java.sql.SQLException;
 
 /**
@@ -12,6 +13,7 @@ public class ExceptionHandler {
      * @param sqlException Exception from which information will be extracted
      */
     public static void handleException(SQLException sqlException) {
-
+        System.out.println(sqlException.getErrorCode() + " " + sqlException.getSQLState() + " " + sqlException.getMessage());
+        sqlException.printStackTrace();
     }
 }
